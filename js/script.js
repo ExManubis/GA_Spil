bca8646642f1eec5a3dc6360339ff0295a48f5e5
Mikkel Jørgensen <95000922+ExManubis@users.noreply.github.com>
2024-11-12 10:49:52 +0100

    feat(javascript): add global contants to js
    Added global contants for query selectors in javascript.

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// GLOBAL CONSTANTS
+const goodShip = document.querySelector('#good_ship')
+const evilShip = document.querySelector('#evil_ship')
+const score = document.querySelector('#score')
+const shield = document.querySelector('#shield')
+const time = document.querySelector('#time')
+
+// EVENT LISTENERS
+window.addEventListener('load', showPage);
+
+// GLOBAL FUNCTIONS
+function showPage() {
+  console.log('Show Page')
+}
+
+// HEALTH
+
+
+// TIME
+
+
+// SCORE
