d8c5a05416e356d56d4737de6e926222c19579b7
Mikkel Jørgensen <95000922+ExManubis@users.noreply.github.com>
2024-11-13 10:37:41 +0100

    feat(javascript): add javascript for game
    Added javascript for initial game mechanics.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 // GLOBAL CONSTANTS
-const goodShip = document.querySelector('#good_ship')
-const evilShip = document.querySelector('#evil_ship')
+const goodShipContainer = document.querySelector('#good_ship_container')
+const goodShipSprite = document.querySelector('#good_ship_sprite')
+const evilShipContainer = document.querySelector('#evil_ship_container')
+const evilShipSprite = document.querySelector('#evil_ship_sprite')
 const score = document.querySelector('#score')
-const shield = document.querySelector('#shield')
+const shield = document.querySelector('#life_board')
 const time = document.querySelector('#time')
 
 // EVENT LISTENERS
@@ -11,12 +13,44 @@ window.addEventListener('load', showPage);
 // GLOBAL FUNCTIONS
 function showPage() {
   console.log('Show Page')
+  startGame()
+  
+  // GAME STARTED
+  function startGame() {
+    console.log('starting game..')
+    let shieldVar = 100
+    let scoreVar = 0
+    score.textContent = 'Score: ' + scoreVar
+    shield.textContent = shieldVar + '%'
+    evilShipContainer.classList.add('fly')
+    evilShipContainer.classList.add('pos1')
+    evilShipContainer.addEventListener('click', clickEvilHandler)
+    
+    // EVIL SHIP 
+    function clickEvilHandler() {
+      console.log('klik')
+      evilShipContainer.removeEventListener('click', clickEvilHandler)
+      evilShipContainer.classList.add('freeze')
+      evilShipSprite.classList.add('kill')
+      evilShipSprite.addEventListener('animationend', evilReset)
+  }
+
+    function evilReset() {
+      evilShipContainer.classList.remove('fly')
+      evilShipContainer.classList.remove('pos1')
+      evilShipContainer.classList.remove('freeze')
+      evilShipSprite.classList.remove('kill')
+      evilShipContainer.offsetLeft
+      evilShipContainer.classList.add('fly')
+      evilShipContainer.classList.add('pos2')
+      evilShipContainer.addEventListener('click', clickEvilHandler)
+    }
 }
 
-// HEALTH
 
+  }
 
 // TIME
+let timeVar = 60
+time.textContent = "Time: " + timeVar
 
-
-// SCORE
